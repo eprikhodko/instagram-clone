e5957b28805ea6ce2768883fea13cf3d5b4e674b
eprikhodko <eprikhodkodev@gmail.com>
2021-04-26 10:24:43 +0300

    Handle login page state

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,15 @@
-import React, {useEffect} from "react"
+import React, {useEffect, useState} from "react"
 import {Link} from "react-router-dom"
 // * star symbol means all. So below we're importing ALL routes as ROUTES from our routes constant in /constants/routes 
 import * as ROUTES from "../constants/routes"
 
 export default function Login() {
+    const [emailAddress, setEmailAddress] = useState("")
+    const[password, setPassword] = useState("")
+
+    const [error, setError] = useState("")
+    const isInvalid = password === "" || emailAddress === ""
+
 
     // change page title, after component did mount
     useEffect(() => {
@@ -27,16 +33,21 @@ export default function Login() {
                         className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2"
                         type="text"
                         placeholder="Email address"
+                        value={emailAddress}
+                        onChange={({target}) => setEmailAddress(target.value)}
                     />
                     <input
                         aria-label="Enter your password"
                         className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2"
                         type="password"
                         placeholder="Password"
+                        value={password}
+                        onChange={({target}) => setPassword(target.value)}
                     />
                     <button
+                        disabled={isInvalid}
                         type="submit"
-                        className={`bg-blue-500 text-white w-full rounded h-8 font-bold`}
+                        className={`bg-blue-500 text-white w-full rounded h-8 font-bold ${isInvalid && "opacity-50"}`}
                     >
                         Log In
                     </button>
